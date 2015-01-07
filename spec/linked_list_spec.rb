58a9a3821cea899cf340e7d088511618ffffe9b3
tophat8855 <hrfarley@gmail.com>
2015-01-07 10:32:31 -0800

    set up specs

---
+++ spec/linked_list_spec.rb
@@ -0,0 +1,31 @@
+require 'spec-helper'
+require 'linked_list'
+
+describe LinkedList do
+  describe "#reverse" do
+    it "reverses two nodes" do
+      list = LinkedList.new(1)
+      list.append(2)
+
+      expect(list.reverse.to_s).to eq("2 > 1")
+    end
+
+    it "reverses three nodes" do
+      list = LinkedList.new(1)
+      list.append(2)
+      list.append(3)
+
+      expect(list.reverse.to_s).to eq("3 > 2 > 1")
+    end
+
+    it "reverses a lot of nodes" do
+      list = LinkedList.new(1)
+      list.append(2)
+      list.append(3)
+      list.append(4)
+      list.append(5)
+
+      expect(list.reverse.to_s).to eq("5 > 4 > 3 > 2 > 1")
+    end
+  end
+end
